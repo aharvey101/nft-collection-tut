4073f7089e842437b2c9031425bf0378f3e0a211
Alexander Harvey <harvey.alex01@gmail.com>
2022-04-07 13:56:58 +1000

    .

--- hardhat-tutorial/constants/index.js
+++ hardhat-tutorial/constants/index.js
@@ -1,6 +1,9 @@
 // Address of the Whitelist Contract that you deployed
 const WHITELIST_CONTRACT_ADDRESS = '0x643dc93Aa0B1F62146E9077B50b08647559836f5'
 // URL to extract Metadata for a Crypto Dev NFT
-const METADATA_URL = 'https://nft-collection-sneh1999.vercel.app/api/'
+// const METADATA_URL =
+//   'https://nft-collection-m57z70uzg-aharvey101.vercel.app/api/'
+const METADATA_URL =
+  'https://nft-collection-m57z70uzg-aharvey101.vercel.app/api/'
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL }
--- hardhat-tutorial/scripts/deploy.js
+++ hardhat-tutorial/scripts/deploy.js
@@ -25,3 +25,11 @@ async function main() {
     deployedCryptoDevsContract.address
   )
 }
+
+// Call the main function and catch if there is any error
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error)
+    process.exit(1)
+  })
